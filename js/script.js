3a6671ac70456059c3e5bc5c9dbbe56f78c33001
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-28 15:36:58 +0100

    struttura + funzione push nella lista

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ var app = new Vue ({
     el: '#app',
     
     data: {
-        
+        note: '',
+        noteList: [],
     },
 
     methods: {
-        
+        pushList: function (){
+            
+            let inputValue = document.getElementById('reset');
+
+            this.noteList.push(this.note);
+            inputValue.value = "";
+            console.log(this.noteList);
+
+        }
     }
 })
+
+Vue.config.devtools = true; 
